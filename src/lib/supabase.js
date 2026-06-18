9d90742a838dc5ee762c8ea0cbd52919988a06b1
Amador Perez Roman <98839181+kamerlond@users.noreply.github.com>
2026-06-18 10:19:07 +0200

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://dbncbyupmvsypsxzidom.supabase.co'
-const SUPABASE_KEY = 'sb-publishable-XB06JzGdZIkS9mJ1ljeP7gSTcPgRBU'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
